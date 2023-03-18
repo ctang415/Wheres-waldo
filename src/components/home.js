b1340b3318d86c67af0145fe83eb1b7222662ecb
ctang415 <carrietang117@gmail.com>
2023-03-18 10:17:27 -0400

    Added game images to home screen from firebase data

--- src/components/home.js
+++ src/components/home.js
@@ -5,29 +5,30 @@ import Scoreboard from "./scoreboard"
 const Home = ({gameData}) => {
     return (
         <div className="home">
-            <span className="home-text">How to Play: Find Waldo and his friends across 3 different images.</span>
-            <span className="home-text">Try to get the fastest time to make it onto the leaderboard!</span>
+            <div className="home-text"> 
+                <div>How to Play: Find Waldo and his friends across 3 different images.</div>
+                <div>Try to get the fastest time to make it onto the leaderboard!</div>
+            </div>
             <div className="home-levels">
                 {gameData.map(item => {
                     return (
                         <div key={item.name} className="home-level">
                             <div className="home-level-images">
-                                <img className="home-level-image" src={item.url} alt="Pictures of characters"></img>
+                                <Link to={`/${item.name}`}>
+                                    <img className="home-level-image" src={item.url} alt="Pictures of characters"></img>
+                                </Link>
                             </div>
                             <div className="home-level-text">
-                                {item.name}
+                                <Link to={`/${item.name}`}>
+                                    {item.name}
+                                </Link>
                             </div>
                         </div>
                     )
                 })}
             </div>
             <div className="buttons">
-                <div>
-                    <Link to="/game"><button className="button">Start Game</button></Link>
-                </div>
-                <div>
-                    <button className="button">Leaderboard</button>
-                </div>
+                    <Link to="/leaderboard"><button className="button">Leaderboard</button></Link>
             </div>
         </div>
     )
