5d8679f53e5eb3a51fa0b7877bfa2d671979ad8e
ctang415 <carrietang117@gmail.com>
2023-04-02 00:13:59 -0400

    Prevented empty submissions for names

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,6 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import { db } from "../firebase-config";
 
 const Modal = ({ gameOver, matchId, timer }) => {
+    const [ color, setColor ] = useState(false)
     const [ data, setData ] = useState([])
     const pathName = useLocation();
 
@@ -17,6 +18,11 @@ const Modal = ({ gameOver, matchId, timer }) => {
     }
     
     const submitHandler = async (e) => {
+        if (data.name === undefined) {
+            setColor(true)
+            return e.preventDefault()
+        }
+        setColor(false)
         const ref = doc(db, 'scores', matchId.id)
         await updateDoc(ref, {data: arrayUnion( { data: data })})
     }
@@ -27,7 +33,7 @@ const Modal = ({ gameOver, matchId, timer }) => {
                 <div className="modal-text">
                     <span>You finished in <span id="modal-timer">{timer}</span> second(s)!</span>
                     <span>Enter your name to be put on the leaderboard:</span>
-                    <input type='text' maxLength="10" onChange={(e) => setData( { name: e.target.value, time: timer, date: getDate() })}></input>
+                    <input className={ color ? 'input-false' : 'input-true'} type='text' maxLength="10" onChange={(e) => setData( { name: e.target.value, time: timer, date: getDate() })}></input>
                     <div className="modal-buttons">
                         <Link to="/leaderboard" state={{ from: pathName }}>
                             <div>
@@ -36,7 +42,7 @@ const Modal = ({ gameOver, matchId, timer }) => {
                         </Link>
                         <Link to="/">
                             <div>
-                                <button className="submit-button">Cancel</button>
+                                <button className="submit-button" onClick={(e) => setColor(false)}>Cancel</button>
                             </div>
                         </Link>
                     </div>
